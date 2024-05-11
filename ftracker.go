3d35b9e44c9f8b2d839a7b3e17d743cc375cf535
Alexander <CaH4ec23@yandex.ru>
2024-05-11 23:51:16 +0300

    sixth commit

--- ftracker.go
+++ ftracker.go
@@ -100,7 +100,7 @@ const (
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	heightInCm := height / cmInM
 	meanSpeedMsec := meanSpeed(action, duration) * kmhInMsec
-	return ((walkingCaloriesWeightMultiplier*weight + (math.Pow(meanSpeedMsec, 2)/heightInCm)*walkingSpeedHeightMultiplier*weight) * duration * minInH)
+	return (((walkingCaloriesWeightMultiplier * weight) + (math.Pow(meanSpeedMsec, 2) / heightInCm)) * walkingSpeedHeightMultiplier * weight * duration * minInH)
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
